a9dea370477e9ac82d6fdbb016d56fc0be9b53d7
ArlenZuno <arlenzuno@gmail.com>
2018-08-09 21:05:12 -0700

    fixing home page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import './home.css'
 
 class Home extends Component {
   render() {
     return (
-      <div>
+      <div className='home white-text'>
         Hello, I am Arlen a freelance web developer from Vancouver, BC. Let's build something...
+
+  <a className='dropdown-trigger btn' dataActivates='dropdown1'>MENU</a>
+
+  <ul id='dropdown1' className='dropdown-content'>
+    <li><a href="#!">one</a></li>
+    <li><a href="#!">two</a></li>
+  </ul>
+
+        <p id='title' className='grey-text'>web developer</p>
+        <p id='initials' className='white-text'>
+          AMZ
+        </p>
       </div>
     )
   }
