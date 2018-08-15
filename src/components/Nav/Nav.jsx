c59394fdc1fec96d5718214e4d75445bf303d2d1
ArlenZuno <arlenzuno@gmail.com>
2018-08-15 14:45:36 -0700

    styled navbar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import M from 'materialize-css/dist/js/materialize.min.js';
+import './nav.css'
 
 class Nav extends Component {
   componentDidMount() {
@@ -11,17 +12,17 @@ class Nav extends Component {
   render() {
     return (
       <div>
-        <nav className="nav-extended">
+        <nav id='navBar' className="nav-extended black">
           <div className="nav-wrapper">
-            <a href="#" data-target="mobile-demo" className="sidenav-trigger"><i className="material-icons">menu</i></a>
+            <a href="#" data-target="mobile-demo" className="sidenav-trigger white-text"><i className="material-icons white-text">menu</i></a>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-              <li><Link to='/work'>WORK</Link></li>
-              <li><Link to='/project'>PROJECTS</Link></li>
-              <li><Link to='/contact'>CONTACT</Link></li>
+              <li><Link to='/work' className='white-text'>WORK</Link></li>
+              <li><Link to='/project' className='white-text'>PROJECTS</Link></li>
+              <li><Link to='/contact' className='white-text'>CONTACT</Link></li>
             </ul>
           </div>
         </nav>
-        <ul className="sidenav" id="mobile-demo">
+        <ul className="sidenav white-text" id="mobile-demo">
           <li><Link to='/'>HOME</Link></li>
           <li><Link to='/work'>WORK</Link></li>
           <li><Link to='/project'>PROJECTS</Link></li>
