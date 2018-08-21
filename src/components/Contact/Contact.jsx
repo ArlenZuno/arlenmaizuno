7d9705f3ce839f6ac524519ce0f15cf6b81c35e1
ArlenZuno <arlenzuno@gmail.com>
2018-08-21 15:17:44 -0700

    added icons that is linked to open to a new window

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,6 +7,12 @@ class Contact extends Component {
       <div className='white-text'>
       <Nav/>
       hi@arlenmaizuno.com
+      <a href="https://github.com/ArlenZuno" target="_blank">
+      <img src='./img/git.png' alt='git'/>
+      </a>
+      <a href='https://www.linkedin.com/in/azuno/' target='_blank'>
+      <img src='./img/link.png' alt='linkedin' />
+      </a>
       </div>
     )
   }
