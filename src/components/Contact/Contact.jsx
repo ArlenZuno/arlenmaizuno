9a19adb20a75461e4c2992e5c79680e3698fc581
ArlenZuno <arlenzuno@gmail.com>
2018-08-23 00:15:12 -0700

    fixed img sizes

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -5,22 +5,23 @@ import './contact.css'
 class Contact extends Component {
   render() {
     return (
-      <div className='white-text'>
-      <Nav/>
-      <div id='email' className='center'>
-      hi@arlenmaizuno.com
+      <div id='contact' className='white-text'>
+        <Nav />
+        <div id='email' className='center'>
+          hi@arlenmaizuno.com
       </div>
-      <br /> <br />
-      <div className='center'>
-      <a href="https://github.com/ArlenZuno" target="_blank">
-      <img src='./img/git.png' alt='git' className='icon'/>
-      </a>
-      <a href='https://www.linkedin.com/in/azuno/' target='_blank'>
-      <img src='./img/link.png' alt='linkedin' className='icon'/>
-      </a>
+        <br /> <br />
+        <div className='center icons'>
+          <a href="https://github.com/ArlenZuno" target="_blank">
+            <img src='./img/git.png' alt='git' height='50rem'/>
+          </a>
+          <a href='https://www.linkedin.com/in/azuno/' target='_blank'>
+            <img src='./img/link.png' alt='linkedin' height='50rem'/>
+          </a>
+        </div>
 
-      <img src='./img/skyline.png' alt='vancity' id='skyline' className='responsive-img'/>
-      </div>
+      <img src='./img/skyline.png' alt='vancity' id='skyline' className='responsive-img' />
+
       </div>
     )
   }
