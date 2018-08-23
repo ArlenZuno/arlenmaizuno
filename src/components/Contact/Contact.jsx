6a7e4c97d903c9b5a7d8357e3f98ff86eece025b
ArlenZuno <arlenzuno@gmail.com>
2018-08-22 23:53:58 -0700

    added skyline img of Vancouver

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -13,11 +13,13 @@ class Contact extends Component {
       <br /> <br />
       <div className='center'>
       <a href="https://github.com/ArlenZuno" target="_blank">
-      <img src='./img/git.png' alt='git'/>
+      <img src='./img/git.png' alt='git' className='icon'/>
       </a>
       <a href='https://www.linkedin.com/in/azuno/' target='_blank'>
-      <img src='./img/link.png' alt='linkedin'/>
+      <img src='./img/link.png' alt='linkedin' className='icon'/>
       </a>
+
+      <img src='./img/skyline.png' alt='vancity' id='skyline' className='responsive-img'/>
       </div>
       </div>
     )
