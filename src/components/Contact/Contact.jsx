d556b6ba120fd9325499887fc8bde5deca502e9d
ArlenZuno <arlenzuno@gmail.com>
2018-08-23 00:21:23 -0700

    added instagram icon and changed text size

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,16 +7,22 @@ class Contact extends Component {
     return (
       <div id='contact' className='white-text'>
         <Nav />
+        <br /> <br />
         <div id='email' className='center'>
           hi@arlenmaizuno.com
-      </div>
+        </div>
         <br /> <br />
         <div className='center icons'>
           <a href="https://github.com/ArlenZuno" target="_blank">
             <img src='./img/git.png' alt='git' height='50rem'/>
           </a>
+          &nbsp; &nbsp;
           <a href='https://www.linkedin.com/in/azuno/' target='_blank'>
             <img src='./img/link.png' alt='linkedin' height='50rem'/>
+          </a>
+          &nbsp; &nbsp;
+          <a href='https://www.instagram.com/vrlenmvi' target='_blank'>
+            <img src='./img/instagram.png' alt='instagram' height='50rem'/>
           </a>
         </div>
 
