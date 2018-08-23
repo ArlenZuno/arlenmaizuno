077951cf2b01e1a730b676f26dbef898b1896bc3
ArlenZuno <arlenzuno@gmail.com>
2018-08-22 17:57:42 -0700

    fixing content placement

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,18 +1,24 @@
 import React, { Component } from 'react'
 import Nav from '../Nav/Nav'
+import './contact.css'
 
 class Contact extends Component {
   render() {
     return (
       <div className='white-text'>
       <Nav/>
+      <div id='email' className='center'>
       hi@arlenmaizuno.com
+      </div>
+      <br /> <br />
+      <div className='center'>
       <a href="https://github.com/ArlenZuno" target="_blank">
       <img src='./img/git.png' alt='git'/>
       </a>
       <a href='https://www.linkedin.com/in/azuno/' target='_blank'>
-      <img src='./img/link.png' alt='linkedin' />
+      <img src='./img/link.png' alt='linkedin'/>
       </a>
+      </div>
       </div>
     )
   }
