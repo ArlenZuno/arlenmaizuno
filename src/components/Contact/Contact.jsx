a77b1217469c0d359bf1e4cf7c57d5bc22862fd9
ArlenZuno <arlenzuno@gmail.com>
2018-08-24 01:54:37 -0700

    edit spacing

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,26 +7,26 @@ class Contact extends Component {
     return (
       <div id='contact' className='white-text'>
         <Nav />
-        <br /> <br />
+        <br /> <br /> <br /> <br />
         <div id='email' className='center'>
           hi@arlenmaizuno.com
         </div>
-        <br /> <br />
+        <br /> <br /> 
         <div className='center icons'>
           <a href="https://github.com/ArlenZuno" target="_blank">
-            <img src='./img/git.png' alt='git' height='50rem'/>
+            <img src='./img/git.png' alt='git' height='50rem' />
           </a>
           &nbsp; &nbsp;
           <a href='https://www.linkedin.com/in/azuno/' target='_blank'>
-            <img src='./img/link.png' alt='linkedin' height='50rem'/>
+            <img src='./img/link.png' alt='linkedin' height='50rem' />
           </a>
           &nbsp; &nbsp;
           <a href='https://www.instagram.com/vrlenmvi' target='_blank'>
-            <img src='./img/instagram.png' alt='instagram' height='50rem'/>
+            <img src='./img/instagram.png' alt='instagram' height='50rem' />
           </a>
         </div>
 
-      <img src='./img/skyline.png' alt='vancity' id='skyline' className='responsive-img' />
+        <img src='./img/skyline.png' alt='vancity' id='skyline' className='responsive-img' />
 
       </div>
     )
